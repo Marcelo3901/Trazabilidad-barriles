1093c49bb95d3795aacc8a2f01e6df632f588ed3
Marcelo3901 <memc2602@gmail.com>
2025-03-12 21:42:25 -0500

    Update trazabilidad_barriles.py

--- trazabilidad_barriles.py
+++ trazabilidad_barriles.py
@@ -89,7 +89,7 @@ if st.button("Guardar Registro"):
             "entry.1545499818": estado_barril,
             "entry.91059345": cliente,
             "entry.1661747572": responsable,
-            "entry.1195378605": observaciones
+            "entry.1195378605": observacionesmov
         }
         response = requests.post(url, data=data)
         if response.status_code == 200:
