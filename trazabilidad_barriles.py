80de4259a4628162f25c14a5c5bfbb2bd99c1be9
Marcelo3901 <memc2602@gmail.com>
2025-03-15 13:04:15 -0500

    Update trazabilidad_barriles.py

--- trazabilidad_barriles.py
+++ trazabilidad_barriles.py
@@ -202,7 +202,7 @@ cantidad_lata_cf = st.number_input("Cantidad de latas", min_value=1, key="cantid
 lote_lata_cf = st.text_input("Lote", key="lote_cf")
 
 if st.button("Guardar Ingreso de Latas al Cuarto Frío"):
-    form_cf_url = "https://docs.google.com/forms/d/e/1FAIpQLSel1h5A4u9f4IwDRtiRKHRLgpqMIYQ29zFv7ChpjybZiV1j4g/viewform?usp=sharing"
+    form_cf_url = "https://docs.google.com/forms/d/e/1FAIpQLSel1h5A4u9f4IwDRtiRKHRLgpqMIYQ29zFv7ChpjybZiV1j4g/viewform"
     payload_cf = {
         "entry.1835972875": estilo_lata_cf,
         "entry.1482364125": str(cantidad_lata_cf),
